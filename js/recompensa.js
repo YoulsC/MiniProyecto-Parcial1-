5c7cdccfdb1608376f62fa1e1f523a10e8a5dd25
Christian Escamilla <capimarquez23@gmail.com>
2024-03-09 15:17:17 -0600

    sonido happy wheels

--- js/recompensa.js
+++ js/recompensa.js
@@ -7,7 +7,6 @@ function dibujarFelicitacion() {
     if (datosRecompensaJSON) {
         const datosRecompensa = JSON.parse(datosRecompensaJSON);
 
-        // Crear elementos HTML para mostrar la felicitación
         const titulo = document.createElement('h1');
         titulo.textContent = '¡Felicidades!';
 
@@ -19,51 +18,40 @@ function dibujarFelicitacion() {
         const puntos = datosRecompensa.puntos;
 
         const detalles = document.createElement('h6');
-        detalles.textContent = `Jugador: ${nombreJugador} - Puntuación: ${puntos} - mejorTiempo: ${datosRecompensa.mejorTiempo} segundos.`;
+        detalles.textContent = `Jugador: ${nombreJugador} - Puntuación: ${puntos} - Tiempo: ${datosRecompensa.mejorTiempo} segundos.`;
         detalles.style.fontFamily = "'Comic Sans MS', cursive, sans-serif";
 
-        // Agregar elementos al contenedor
         container.appendChild(titulo);
         container.appendChild(mensaje);
         container.appendChild(detalles);
 
-        // Agregar fondo de la jungla
-        container.style.backgroundImage = "url('../media/thumbnail.jpg')"; // Reemplaza 'ruta/a/la/imagen/jungla.jpg' con la ruta a tu imagen de la jungla
+        container.style.backgroundImage = "url('../media/thumbnail.jpg')"; 
         container.style.backgroundSize = 'cover';
         container.style.backgroundPosition = 'center bottom';
 
-        // Reproducir música de ganador
-        const audio = new Audio('../media/sonidos/victory.mp3'); // Reemplaza 'ruta/a/la/musica/ganador.mp3' con la ruta a tu archivo de música
+        const audio = new Audio('../media/sonidos/victory.mp3'); 
         audio.play();
 
-        // Obtener el alias y la puntuación del jugador actual
         const aliasActual = datosRecompensa.alias;
         const puntosActual = datosRecompensa.puntos;
-        const mejorTiempoActual = datosRecompensa.mejorTiempo; // Suponiendo que también hay un campo de mejorTiempo en los datos
-
-        // Verificar si hay datos previos para el mismo jugador en el almacenamiento local
+        const mejorTiempoActual = datosRecompensa.mejorTiempo; 
         const datosPreviosJSON = localStorage.getItem(aliasActual);
 
         if (datosPreviosJSON) {
             const datosPrevios = JSON.parse(datosPreviosJSON);
 
-            // Verificar si la puntuación actual es mayor o igual a la puntuación previa
             if (puntosActual > datosPrevios.puntos || 
                 (puntosActual === datosPrevios.puntos && mejorTiempoActual < datosPrevios.mejorTiempo)) {
-                // Actualizar los datos en el almacenamiento local
                 localStorage.setItem(aliasActual, JSON.stringify(datosRecompensa));
             }
         } else {
-            // Si no hay datos previos para este jugador, almacenar los datos actuales
             localStorage.setItem(aliasActual, JSON.stringify(datosRecompensa));
         }
     } else {
-        // Mostrar un mensaje de error si no se encuentran datos en el almacenamiento local
         container.textContent = 'No se encontraron datos de recompensa.';
     }
 }
 
-// Llamamos a la función para dibujar la felicitación cuando se carga el contenido
 document.addEventListener('DOMContentLoaded', function() {
     dibujarFelicitacion();
 
@@ -86,6 +74,6 @@ document.addEventListener('DOMContentLoaded', function() {
     verReportesButton.addEventListener('click', function() {
         // Redireccionar a la página de reportes
         localStorage.removeItem('datosRecompensa');
-        window.location.href = 'reportes.html'; // Reemplaza 'reportes.html' con la ruta correcta a la página de reportes
+        window.location.href = 'reportes.html'; 
     });
 });
